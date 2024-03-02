291fd3c9c58c7e24f85d58b0d12c9bcca6321fff
ramrajsainiiitd <109168257+ramrajsainiiitd@users.noreply.github.com>
2024-03-02 12:26:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 function shortcut(s1, s2) {
   // your code here
+	if (s1.length>0&&s2.length>0) {
+		console.log("ramraj")
+return s1[0]+s2[0];		
+	}
+	else {
+		return "";
+	}
 }
 
 // Do not change the code below.
-const s1 = prompt("Enter s1:");
+const s1 = prompt("Enter s1:"); 
 const s2 = prompt("Enter s2:");
-alert(shortcut(s1, s2));
+ alert(shortcut(s1, s2));
